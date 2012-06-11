323139a2ffa731469178a0b8e35fd450bbcb2622
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2012-06-11 10:47:56 +0200

    Use args4j for parsing CLI arguments

---
+++ src/main/java/de/krkm/patterndebug/ui/cmdline/CLIOptions.java
@@ -0,0 +1,17 @@
+package de.krkm.patterndebug.ui.cmdline;
+
+import org.kohsuke.args4j.Option;
+
+import java.io.File;
+
+/**
+ * Defines command line parameters usable for starting the reasoner. Options parsing is done via args4j.
+ */
+public class CLIOptions {
+    public File getInputOntology() {
+        return inputOntology;
+    }
+
+    @Option(name="-i", usage="Ontology to perform reasoning on", required=true)
+    private File inputOntology;
+}
